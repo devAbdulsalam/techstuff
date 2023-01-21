1866ad9c38f007707e086c9efae07fb58ec02b3e
my-amtech <ammuftau74@gmail.com>
2023-01-21 18:28:58 +0100

    another commit

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const userRoutes = require('./routes/user')
 const app = express();
 app.use(
     cors({
-        origin: ["http://localhost:3000", "https://shara-app.vercel.app",],
+        origin: ["http://localhost:3000", "https://shara.onrender.com"],
         credentials: true,
     })
 );
